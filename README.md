30ebbd9a4852ab342513c3c500bf2c33684c0ffd
Zachary  Stark Jones <zachary.starkjones@fynanz.com>
2012-02-25 19:44:23 -0500

    remove gangle -repo string from install path

--- README.md
+++ README.md
@@ -16,13 +16,13 @@ Vim Configs aims to make a 2-command install of a full vim harnass and CLI conve
 ## Install ##
 Download and unpack to a ```.```directory in your home.
 
-``` cd ~; git clone git://github.com/NewAlexandria/vim.ana.git .vim.ana-repo ```
+``` cd ~; git clone git://github.com/NewAlexandria/vim.ana.git .vim.ana ```
 
 then link files into your home
 
-``` .vim.ana-repo/install.sh ```
+``` .vim.ana/install.sh ```
 
-The installer responsibily makes backups of your original .vim files to ~/.vim.ana-repo/backups/  I thought this was better than to ```cp -R``` the lot of files into your existing .vim   
+The installer responsibily makes backups of your original .vim files to ~/.vim.ana/backups/  I thought this was better than to ```cp -R``` the lot of files into your existing .vim   
 
 That's it for now.
 
--- install.sh
+++ install.sh
@@ -1,35 +1,35 @@
 # Backup any existing files
-mkdir ~/.vim.ana-repo/backups
+mkdir ~/.vim.ana/backups
 
 ## Link vim files
 if [ -d ~/.vim ] 
 then
   echo "made a backup of ~/.vim" 
-  mv ~/.vim ~/.vim.ana-repo/backups/.vim
+  mv ~/.vim ~/.vim.ana/backups/.vim
 fi
-ln -s ~/.vim.ana-repo/.vim ~/.vim
+ln -s ~/.vim.ana/.vim ~/.vim
 
 if [ -f ~/.vimrc ]
 then
   echo "made a backup of ~/.vimrc" 
-  mv ~/.vimrc ~/.vim.ana-repo/backups/.vimrc
+  mv ~/.vimrc ~/.vim.ana/backups/.vimrc
 fi
-ln -s ~/.vim.ana-repo/.vimrc ~/.vimrc
+ln -s ~/.vim.ana/.vimrc ~/.vimrc
 
 if [ -f ~/.viminfo ]
 then
   echo "made a backup of ~/.viminfo" 
-  mv ~/.viminfo ~/.vim.ana-repo/backups/.viminfo
+  mv ~/.viminfo ~/.vim.ana/backups/.viminfo
 fi
-ln -s ~/.vim.ana-repo/.viminfo ~/.viminfo
+ln -s ~/.vim.ana/.viminfo ~/.viminfo
 
 ## link the git funcs for the prompt
 if [ -f ~/.git-completion.bash ]
 then
-  mv  ~/.git-completion.bash ~/.vim.ana-repo/backups/.git-completion.bash
+  mv  ~/.git-completion.bash ~/.vim.ana/backups/.git-completion.bash
 fi
 echo "linking github's supported libs"
-ln -s ~/.vim.ana-repo/.git-completion.bash  ~/.git-completion.bash
+ln -s ~/.vim.ana/.git-completion.bash  ~/.git-completion.bash
 
 ## Improve the prompt
 SOURCE_LINE="source ~/.bash_vim_append"
@@ -39,7 +39,7 @@ if [[ -f "$PROFILE" ]] && grep -q "$SOURCE_LINE" "$PROFILE"; then
   echo "Already added to bash profile."
 else
   echo "Adding .vim.ana to your bash profile..."
-  ln -s ~/.vim.ana-repo/.bash_vim_append ~/.bash_vim_append
+  ln -s ~/.vim.ana/.bash_vim_append ~/.bash_vim_append
 
   echo "# .vim.ana configurations #" >> "$PROFILE"
   echo "$SOURCE_LINE" >> "$PROFILE"
